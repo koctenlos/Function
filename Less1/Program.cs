1dc76d023f8f582cdd19eb75ff6ee92fb162df7b
PC-1 <danilnub01@gmail.com>
2022-09-23 20:05:05 +0700

    add Less1

---
+++ Less1/Program.cs
@@ -0,0 +1,43 @@
+﻿// int x=10;
+
+// if (x > 5)
+// {
+//     Console.WriteLine(x);
+// }
+
+// while (x>5)
+// {
+//     Console.WriteLine(x);
+//     x=x-1;
+// }
+
+// for (int i=1; i<10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+Console.Clear();
+int x = new Random().Next(99, 1000);
+Console.WriteLine(x);
+
+int temp = x % 10;
+int temp2= x/100;
+
+Console.Write(temp2);
+Console.WriteLine(temp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

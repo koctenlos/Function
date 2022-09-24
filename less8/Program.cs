39d51d732f8a42564795e6106da95092ca2200ea
PC-1 <danilnub01@gmail.com>
2022-09-24 16:34:20 +0700

    add Less8

---
+++ less8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая 
+// принимает на вход цифру, обозначающую день 
+// недели, и проверяет, является ли 
+// этот день выходным.
+
+Console.Clear();
+Console.Write("Введите цифру дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a>=1 & a<=5){
+    Console.WriteLine("Нет ");
+}
+else if (a>=5 & a<=7){
+    Console.WriteLine("Да");
+}
+else if (a<1 | a>7){
+    Console.WriteLine("Неверно указан день");
+}
